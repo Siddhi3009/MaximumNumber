39ceeeb9b92efaffd044910b1dfb38cd06e44c3a
Administrator <siddhiseth3009@gmail.com>
2020-10-20 16:31:10 +0530

    [Siddhi]Add. GetMaximumInteger method added

---
+++ GetMaximumNumber/MaximumNumberCheck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GetMaximumNumber
+{
+    class MaximumNumberCheck
+    {
+        public static int MaximumIntegerNumber(int num1, int num2, int num3)
+        {
+            try
+            {
+                if ((num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0) || (num1.CompareTo(num2) >= 0 && num1.CompareTo(num3) > 0) || (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) >= 0))
+                {
+                    return num1;
+                }
+                if ((num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0) || (num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0) || (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0))
+                {
+                    return num2;
+                }
+                if ((num3.CompareTo(num2) > 0 && num3.CompareTo(num1) > 0) || (num3.CompareTo(num2) >= 0 && num3.CompareTo(num1) > 0) || (num3.CompareTo(num2) > 0 && num3.CompareTo(num1) >= 0))
+                {
+                    return num3;
+                }
+                else
+                    throw new Exception("firstNumber,secondNumber and thirdNumber are same");
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+                return -1;
+            }
+        }
+    }
+}
--- GetMaximumNumber/Program.cs
+++ GetMaximumNumber/Program.cs
@@ -7,6 +7,9 @@ namespace GetMaximumNumber
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Get Maximum Number Program");
+            Console.WriteLine("Enter Numbers");
+            int output = MaximumNumberCheck.MaximumIntegerNumber(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            Console.WriteLine("Maximum Number: " + output);
         }
     }
 }
