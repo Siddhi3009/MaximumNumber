a0f4059976ef12db4009d79d833a03c780858d0d
Administrator <siddhiseth3009@gmail.com>
2020-10-20 19:52:20 +0530

    [Siddhi]Add. Generic class added

---
+++ GetMaximumNumber/GenericMaximumCheck.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace GetMaximumNumber
+{
+    public class GenericMaximumCheck<T> where T : IComparable
+    {
+        public T num1;
+        public T num2;
+        public T num3;
+
+        public GenericMaximumCheck(T num1, T num2, T num3)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+            this.num3 = num3;
+        }
+        public T MaximumCheck()
+        {
+            if ((this.num1.CompareTo(this.num2) > 0 && this.num1.CompareTo(this.num3) > 0) || (this.num1.CompareTo(this.num2) >= 0 && this.num1.CompareTo(this.num3) > 0) || (this.num1.CompareTo(this.num2) > 0 && this.num1.CompareTo(this.num3) >= 0))
+            {
+                return this.num1;
+            }
+            if ((this.num2.CompareTo(this.num1) > 0 && this.num2.CompareTo(this.num3) > 0) || (this.num2.CompareTo(this.num1) >= 0 && this.num2.CompareTo(this.num3) > 0) || (this.num2.CompareTo(this.num1) > 0 && this.num2.CompareTo(this.num3) >= 0))
+            {
+                return this.num2;
+            }
+            if ((this.num3.CompareTo(this.num2) > 0 && this.num3.CompareTo(this.num1) > 0) || (this.num3.CompareTo(this.num2) >= 0 && this.num3.CompareTo(this.num1) > 0) || (this.num3.CompareTo(this.num2) > 0 && this.num3.CompareTo(this.num1) >= 0))
+            {
+                return this.num3;
+            }
+
+            throw new Exception("first value,second value and third value are same");
+        }
+        public void PrintMaxValue()
+        {
+            var output = MaximumCheck();
+            Console.WriteLine("Maximum value: " + output);
+        }
+    }
+}
--- GetMaximumNumber/Program.cs
+++ GetMaximumNumber/Program.cs
@@ -6,17 +6,33 @@ namespace GetMaximumNumber
     {
         static void Main(string[] args)
         {
+            //Generic Class
+
             Console.WriteLine("Welcome to Get Maximum Number Program");
             Console.WriteLine("Enter values to be compared");
-            int output1 = MaximumNumberCheck.MaximumCheck<int>(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
-            Console.WriteLine("Maximum value: " + output1);
+            GenericMaximumCheck<int> genericInt = new GenericMaximumCheck<int>(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            genericInt.PrintMaxValue();
             Console.WriteLine("Enter values to be compared");
-            float output2 = MaximumNumberCheck.MaximumCheck<float>(float.Parse(Console.ReadLine()), float.Parse(Console.ReadLine()), float.Parse(Console.ReadLine()));
-            Console.WriteLine("Maximum value: " + output2);
+            GenericMaximumCheck<float> genericFloat = new GenericMaximumCheck<float>(float.Parse(Console.ReadLine()), float.Parse(Console.ReadLine()), float.Parse(Console.ReadLine()));
+            genericFloat.PrintMaxValue();
             Console.WriteLine("Enter values to be compared");
-            string output3 = MaximumNumberCheck.MaximumCheck<string>(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
-            Console.WriteLine("Maximum value: " + output3);
-            
+            GenericMaximumCheck<string> genericString = new GenericMaximumCheck<string>(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            genericString.PrintMaxValue();
+
+            //GenericMethod
+
+            //Console.WriteLine("Enter values to be compared");
+            //int output1 = MaximumNumberCheck.MaximumCheck<int>(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
+            //Console.WriteLine("Maximum value: " + output1);
+            //Console.WriteLine("Enter values to be compared");
+            //float output2 = MaximumNumberCheck.MaximumCheck<float>(float.Parse(Console.ReadLine()), float.Parse(Console.ReadLine()), float.Parse(Console.ReadLine()));
+            //Console.WriteLine("Maximum value: " + output2);
+            //Console.WriteLine("Enter values to be compared");
+            //string output3 = MaximumNumberCheck.MaximumCheck<string>(Console.ReadLine(), Console.ReadLine(), Console.ReadLine());
+            //Console.WriteLine("Maximum value: " + output3);
+
+            //Normal Method
+
             //Console.WriteLine("Enter integer Numbers");
             //int output1 = MaximumNumberCheck.MaximumIntegerNumber(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()));
             //Console.WriteLine("Maximum integer Number: " + output1);
