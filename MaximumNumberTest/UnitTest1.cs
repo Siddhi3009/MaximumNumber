4d268abd4e6ecdfcbc5ffa216b6a59bf6f3ca189
Administrator <siddhiseth3009@gmail.com>
2020-10-20 19:13:07 +0530

    [Siddhi]Add. Test cases for generic method added

--- MaximumNumberTest/UnitTest1.cs
+++ MaximumNumberTest/UnitTest1.cs
@@ -69,20 +69,20 @@ namespace MaximumNumberTest
         [DataRow("9", "14", "8")]
         [DataRow("99", "65", "74")]
 
-        public void GivenMaxValueAtFirstPlace_WhenCheckedInMaximumString_ShouldReturnFirstNumber(string num1, string num2, string num3)
+        public void GivenMaxValueAtFirstPlace_WhenCheckedInMaximumString_ShouldReturnFirstNumber(string string1, string string2, string string3)
         {
-            string expected = num1;
-            string actual = MaximumNumberCheck.MaximumString(num1, num2, num3);
+            string expected = string1;
+            string actual = MaximumNumberCheck.MaximumString(string1, string2, string3);
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
         [DataRow("14", "9", "8")]
         [DataRow("65", "99", "74")]
 
-        public void GivenMaxValueAtSecondPlace_WhenCheckedInMaximumString_ShouldReturnSecondNumber(string num1, string num2, string num3)
+        public void GivenMaxValueAtSecondPlace_WhenCheckedInMaximumString_ShouldReturnSecondNumber(string string1, string string2, string string3)
         {
-            string expected = num2;
-            string actual = MaximumNumberCheck.MaximumString(num1, num2, num3);
+            string expected = string2;
+            string actual = MaximumNumberCheck.MaximumString(string1, string2, string3);
             Assert.AreEqual(expected, actual);
         }
         [TestMethod]
@@ -93,6 +93,36 @@ namespace MaximumNumberTest
         {
             string expected = string3;
             string actual = MaximumNumberCheck.MaximumString(string1, string2, string3);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        [DataRow("9", "14", "8")]
+        [DataRow("99", "65", "74")]
+
+        public void GivenMaxValueAtFirstPlace_WhenCheckedInMaximumCheck_ShouldReturnFirstValue(string num1, string num2, string num3)
+        {
+            string expected = num1;
+            string actual = MaximumNumberCheck.MaximumCheck<string>(num1, num2, num3);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        [DataRow(9, 14, 8)]
+        [DataRow(65, 99, 74)]
+
+        public void GivenMaxValueAtSecondPlace_WhenCheckedInMaximumCheck_ShouldReturnSecondValue(int num1, int num2, int num3)
+        {
+            int expected = num2;
+            int actual = MaximumNumberCheck.MaximumCheck<int>(num1, num2, num3);
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        [DataRow(14.23F, 8.36F, 99.99F)]
+        [DataRow(65.36F, 9.56F, 74.56F)]
+
+        public void GivenMaxValueAtThirdPlace_WhenCheckedInMaximumCheck_ShouldReturnThirdValue(float num1, float num2, float num3)
+        {
+            float expected = num3;
+            float actual = MaximumNumberCheck.MaximumCheck<float>(num1, num2, num3);
             Assert.AreEqual(expected, actual);
         }
     }
