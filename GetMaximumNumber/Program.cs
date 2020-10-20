b5bece5a6da6eee5d9edd1f687245173cdba7cdb
Administrator <siddhiseth3009@gmail.com>
2020-10-20 16:00:11 +0530

    [Siddhi]Add. Welcome message added

---
+++ GetMaximumNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GetMaximumNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Get Maximum Number Program");
+        }
+    }
+}
